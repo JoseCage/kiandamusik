b8cd2045123c812be15d7226a7e17c3433841302
José Cage <josejanuario7@gmail.com>
2017-12-14 18:55:43 +0100

    Created Principal Activity to show popular artists

---
+++ app/src/main/java/me/mentedigital/kiandamusik/PrincipalActivity.java
@@ -0,0 +1,13 @@
+package me.mentedigital.kiandamusik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
