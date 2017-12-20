106d4c00a45e180ee695eeba4753e27e0164030c
José Cage <josejanuario7@gmail.com>
2017-12-20 16:50:50 +0100

    :muscle: add timer to splashscreen

--- app/src/main/java/me/mentedigital/kiandamusik/SplashScreenActivity.java
+++ app/src/main/java/me/mentedigital/kiandamusik/SplashScreenActivity.java
@@ -1,13 +1,36 @@
 package me.mentedigital.kiandamusik;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
+    Context content = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(content, PrincipalActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                        .setFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                startActivity(intent);
+                finish();
+            }
+        };
+        timer.schedule(task, 3000L);
     }
 }
