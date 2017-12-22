d95295ff1a26093817eb73ac95bf9533a672f955
José Cage <josejanuario7@gmail.com>
2017-12-22 16:09:37 +0100

    :muscle: Add about screen and menu

--- app/src/main/java/me/mentedigital/kiandamusik/AboutActivity.java
+++ app/src/main/java/me/mentedigital/kiandamusik/AboutActivity.java
@@ -2,6 +2,7 @@ package me.mentedigital.kiandamusik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -9,5 +10,23 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        if (getSupportActionBar() != null) {
+            setTitle(getString(R.string.acerca));
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
